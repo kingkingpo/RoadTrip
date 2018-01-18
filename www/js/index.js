87470fdbe5557b7e2a04a1a6939725814d4ee3a0
kingkingpo <kingkingpo@naver.com>
2018-01-18 18:41:53 -0500

    login

--- www/js/index.js
+++ www/js/index.js
@@ -29,3 +29,48 @@ function addLocations(){
      
     
  }
+
+ firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+      $(".login-cover").hide();
+
+      var dialog = document.querySelector('#loginDialog');
+      if (! dialog.showModal) {
+        dialogPolyfill.registerDialog(dialog);
+      }
+      dialog.close();
+     
+    } else {
+      // No user is signed in.
+      var dialog = document.querySelector('#loginDialog');
+      if (! dialog.showModal) {
+        dialogPolyfill.registerDialog(dialog);
+      }
+      dialog.showModal();
+     
+    }
+  });
+
+$("#loginBtn").click(
+    function(){
+        var email = $("#loginEmail").val();
+        var password = $("#loginPassword").val();
+
+        if(email != "" && password !=""){
+            $("#loginProgress").show();
+            $("#loginBtn").hide();
+
+            firebase.auth().signInWithEmailAndPassword(email,password).catch(function(error){
+                $("#loginError").show().text(error.message);
+
+                $("#loginProgress").hide();
+                $("#loginBtn").show();
+            });
+
+
+        }
+
+
+    }
+);
